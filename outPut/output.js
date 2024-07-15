8e9a6e3af7abaf4aa98a48b6a98bf0e6ebf4e8bd
abhishek121205 <khacharabhi3@gmail.com>
2024-07-15 09:42:02 +0530

    output

---
+++ outPut/output.js
@@ -0,0 +1,5 @@
+console.log('helloo.....')
+window.alert('hellooo')
+document.write('hellooo....')
+document.getElementById("abc").innerHTML = ('hell00oo..');
+document.getElementById("xyz").innerHTML = ('hell00oo..');
