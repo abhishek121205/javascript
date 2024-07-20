b9cecafaf48156c3def21441d042e7a241a3b711
abhishek121205 <khacharabhi3@gmail.com>
2024-07-20 21:36:48 +0530

    nesting in js

---
+++ nested/if_else_nested.js
@@ -0,0 +1,12 @@
+var user = "hello";
+var pass = 1212;
+
+if (user == "hello") {
+  if (pass == 1212) {
+    console.log("you logedin..");
+  } else {
+    console.log("incorrect password..");
+  }
+} else {
+  console.log("incorrect username");
+}
